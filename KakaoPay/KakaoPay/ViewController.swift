de0f86a33fcfd2a6fa0aabea38a5d7e06f47078a
KooMinJun <mjkoo711@gmail.com>
2019-08-01 01:17:15 +0900

    set default project environment

---
+++ KakaoPay/KakaoPay/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KakaoPay
+//
+//  Created by 구민준 on 01/08/2019.
+//  Copyright © 2019 mjkoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
