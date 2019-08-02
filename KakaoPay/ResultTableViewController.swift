1ad2549147a9e231a5724796c86ac0a077d23ac2
koominjun <mjkoo711@gmail.com>
2019-08-02 16:40:46 +0900

    search for locations using MKLocalSearchRequest

--- KakaoPay/ResultTableViewController.swift
+++ KakaoPay/ResultTableViewController.swift
@@ -7,23 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
 class ResultTableViewController: UITableViewController {
+  var matchingItems: [MKMapItem] = []
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return 0
+  func parseAddress(selectedItem: MKPlacemark) -> String {
+    let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+    let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil) || (selectedItem.administrativeArea != nil) ? ", " : ""
+    let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+    let addressLine = String(format: "%@%@%@%@%@%@%@", selectedItem.subThoroughfare ?? "", firstSpace, selectedItem.thoroughfare ?? "", comma, selectedItem.locality ?? "", secondSpace, selectedItem.administrativeArea ?? "")
+    return addressLine
+  }
+}
+
+extension ResultTableViewController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return matchingItems.count
   }
 
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell")!
+    let selectedItem = matchingItems[indexPath.row].placemark
+    cell.textLabel?.text = selectedItem.name
+    cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
+    return cell
   }
 }
 
 extension ResultTableViewController: UISearchResultsUpdating {
   func updateSearchResults(for searchController: UISearchController) {
-    // TODO
+    guard let searchBarText = searchController.searchBar.text else { return }
+    let request = MKLocalSearch.Request()
+    request.naturalLanguageQuery = searchBarText
+    request.region = MKCoordinateRegion()
+    let search = MKLocalSearch(request: request)
+    search.start { (response, error) in
+      guard let response = response else { return }
+      self.matchingItems = response.mapItems
+      self.tableView.reloadData()
+    }
   }
 }
